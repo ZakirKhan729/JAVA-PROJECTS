7b832a40b900241df4460693d4ff9512a2fca079
ZakirKhan729 <khnzkr07@gmail.com>
2024-05-24 18:46:15 +0530

    Added a Calculator program

---
+++ Calculator.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ans = 0;
+        while (true) {
+            System.out.print("Enter the operator: ");
+            char op = sc.next().trim().charAt(0);
+            if ((op == '+') || (op == '-') || (op == '*') || (op == '/') || (op == '%')) {
+                System.out.print("Enter 1st number: ");
+                int num1 = sc.nextInt();
+                System.out.print("Enter 2nd number: ");
+                int num2 = sc.nextInt();
+                if (op == '+') {
+                    ans = num1 + num2;
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+                if (op == '/') {
+                    if (num2 != 0) {
+                        ans = num1 / num2;
+                    }
+                }
+                if (op == '%') {
+                    if (num2 != 0) {
+                        ans = num1 % num2;
+                    }
+                }
+            }else if ((op=='x') || (op=='X')) {
+                break;
+            }else {
+                System.out.println("Invalid operation");
+            }
+            System.out.println("The answer is: "+ans);
+        }
+    }
+}
